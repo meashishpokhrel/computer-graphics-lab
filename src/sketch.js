e90e58fe02a01f773754483499b8ee3e75152bbc
meashishpokhrel <meashishpokhrel@gmail.com>
2020-07-13 18:38:06 +0545

    3D animation

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,24 @@
+let angle = 0
+
 function setup() {
-  createCanvas (400, 400)
+  createCanvas (400, 400, WEBGL)
 }
 
 function draw() {
-  background (255, 0, 0)
-  ellipse (200, 200, 50 ,50)
+  background (90)
+  rectMode(CENTER)
+  noStroke()
+  fill (0, 0,255)
+  rotateX(angle)
+  rotateY(angle * 0.4)
+  rect (0,0,150,150)
+  angle += 0.07
 }
+
+// background (175)
+//   rectMode(CENTER)
+//   noStroke()
+//   fill (0, 0,255)
+//   rotateX(angle)
+//   rect (0,0,150,150)
+//   angle += 0.07
